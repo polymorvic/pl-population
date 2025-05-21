264f7ac3f3c01412d1e8e098ec95cf9f17adff3b
polymorvic <polymorphic.bite@gmail.com>
2025-05-21 22:01:11 +0200

    update source data processing

--- src/utils/preprocessing.py
+++ src/utils/preprocessing.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+from typing import Optional
+from .const import GUS_COLNAMES
 
 class ExcelLoaderMixin:
     """Simple mixin to load Excel files."""
@@ -7,22 +10,45 @@ class ExcelLoaderMixin:
 
 
 class PopulationDataProcessor(ExcelLoaderMixin):
-    def __init__(self, historical_path, recent_path):
+    def __init__(self, historical_path: str, recent_path: str):
         self.historical_path = historical_path
         self.recent_path = recent_path
-        self.data = None
+        self.historical_df: Optional[pd.DataFrame] = None
+        self.recent_df: Optional[pd.DataFrame] = None
 
-    def process(self):
-        historical_df = self.load_excel(self.historical_path, skiprows=5).iloc[1:75, 0:2]
-        historical_df.columns = ['rok', 'populacja']
+    def __process_historical(self) -> None:
+        cols_idxs = np.r_[0:10, 11, 13, 14]
+        self.historical_df = self.load_excel(self.historical_path, skiprows=5).iloc[1:75, cols_idxs]
+        self.historical_df.columns = GUS_COLNAMES
 
-        recent_df = self.load_excel(self.recent_path, sheet_name='Tabl. 1', skiprows=2)
-        population_2024 = int(recent_df.iloc[1, 4] / 1000)
+        for col in self.historical_df.columns[1:]:
+            s = self.historical_df[col]
+            if not pd.api.types.is_numeric_dtype(s):
+                s = pd.to_numeric(s.str.replace('[a-zA-Z]', '', regex=True).str.replace(',', '.'), errors='coerce')
 
-        historical_df.loc[len(historical_df)] = ['2024', population_2024]
-        self.data = historical_df
+    def __process_recent(self) -> None:
+        df = self.load_excel(self.recent_path, skiprows=5).iloc[:, [0, 1, 15]]
+        df.columns = ['rok', 'populacja', 'przyrost_naturalny']
 
-    def get_data(self):
-        if self.data is None:
-            raise ValueError("Data not processed yet. Call `process()` first.")
-        return self.data
+        df = df[df['rok'].astype(str).str.startswith('2024')]
+
+        for col in ['populacja', 'przyrost_naturalny']:
+            if pd.api.types.is_object_dtype(df[col]):
+                df[col] = pd.to_numeric(df[col], errors='coerce')
+
+        df['rok'] = '2024'
+
+        self.recent_df = df.groupby('rok', as_index=False).agg({
+            'populacja': lambda x: int(np.mean(x.dropna())), 
+            'przyrost_naturalny': 'sum'
+        })
+
+    def get_data(self) -> tuple[pd.DataFrame, pd.DataFrame]:
+        try:
+            self.__process_historical()
+            self.__process_recent()
+        except Exception as error:
+            print(f'Something went wrong processing data - {error}')
+            return tuple(pd.DataFrame() for _ in range(2))
+        
+        return self.historical_df, pd.concat([self.historical_df, self.recent_df], ignore_index=True)[['rok', 'populacja', 'przyrost_naturalny']]
